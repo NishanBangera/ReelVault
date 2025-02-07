88ae30164c14dabfdfc68c42b7bdd3a16d9c0268
Nishan Bangera <nishanbangera4321@gmail.com>
2025-02-07 11:21:31 +0530

    feature(auth): Implement NextAuth for authentication

---
+++ lib/auth.ts
@@ -0,0 +1,67 @@
+import { NextAuthOptions } from "next-auth";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { connectToDatabase } from "./db";
+import User from "@/models/User";
+import bcrypt from "bcryptjs";
+
+export const config: NextAuthOptions = {
+  providers: [
+    CredentialsProvider({
+      name: "Credentials",
+      credentials: {
+        email: { label: "Email", type: "email" },
+        password: { label: "Password", type: "password" },
+      },
+      async authorize(credentials) {
+        if (credentials == null) return null;
+
+        try {
+          await connectToDatabase();
+        } catch (error) {
+          throw error;
+        }
+        const user = await User.findOne({ email: credentials.email });
+        if (user && user.password) {
+          const isMatch = await bcrypt.compare(
+            credentials.password as string,
+            user.password
+          );
+
+          //If password is correct, return user
+          if (isMatch) {
+            return {
+              id: user._id.toString(),
+              email: user.email,
+            };
+          }
+        }
+      },
+    }),
+  ],
+  callbacks: {
+    async jwt({ token, user }) {
+      if (user) {
+        token.id = user.id;
+      }
+      return token;
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string;
+      }
+
+      return session;
+    },
+  },
+  pages: {
+    signIn: "/login",
+    error: "/login",
+  },
+  session: {
+    strategy: "jwt",
+    maxAge: 30 * 24 * 60 * 60,
+  },
+  secret: process.env.NEXTAUTH_SECRET
+};
+
+
